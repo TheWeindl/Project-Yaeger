012ed96084bad65182be24783705f8f4033d01cb
TheWeindl <theweindl@gmail.com>
2017-06-13 21:47:40 +0200

    bug fix

--- updateRessources.php
+++ updateRessources.php
@@ -1,17 +1,15 @@
 <?php
 Require_once('config.php');
 
-function UpdateRessources()
-{
-	if(!$oMysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE)
-	{
+function UpdateRessources(){
+	
+	if(!$oMysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE){
 		echo("Could not connect to database");
 	}
 
 	$Result = $oMysqli->query("SELECT lastrefresh FROM userinfo WHERE userID = 2");
 
-	if($Result && $Result->num_rows > 0)
-	{
+	if($Result && $Result->num_rows > 0){
 		$lastRefresh = mysqli_fetch_assoc($Result);
 		
 		$currentRefresh = date("Y-m-d H:i:s");
