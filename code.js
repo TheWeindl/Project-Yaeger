ce2a0b5e5db721a9738e681e0c6c29bcb15deec6
prindom <d.prinzensteiner@gmail.com>
2017-06-13 11:41:24 +0200

    changes for resources animation

--- code.js
+++ code.js
@@ -1,3 +1,33 @@
 /**
  * Created by Dominik on 13.06.2017.
  */
+let $pbMetal = $("#progressBarMetal");
+let $pbWood = $("#progressBarWood");
+let $pbStone = $("#progressBarStone");
+
+let interval = setInterval(makeProgress, 600);
+let val = 0;
+
+function makeProgress() {
+    if(val > 100) {
+        console.log("ended");
+        clearInterval(interval);
+    } else if(val == 100) {
+
+    } else {
+        $pbMetal.attr('aria-valuenow', val);
+        $pbMetal.css("width", val+"%");
+        //$("#progressBarMetal").text(val+"%");
+        $("#metal-head").text("Metal - " + val + "%");
+
+        $pbWood.attr('aria-valuenow', val);
+        $pbWood.css("width", val+"%");
+        $("#wood-head").text("Wood - " + val + "%");
+
+        $pbStone.attr('aria-valuenow', val);
+        $pbStone.css("width", val+"%");
+        $("#stone-head").text("Stone - " + val + "%");
+        val++;
+        // console.log(val)
+    }
+}
--- index.php
+++ index.php
@@ -8,7 +8,45 @@
 </head>
 
 <body>
-Content of the document......
+
+
+
+<div class="panel-group">
+    <div class="panel panel-default panel-res">
+        <div class="panel-heading" id="metal-head">Metal</div>
+        <div class="panel-body">
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped active" id="progressBarMetal" role="progressbar" aria-valuenow="0"
+                     aria-valuemin="0" aria-valuemax="100" style="width:0%">
+                    <span class="sr-only">70% Complete</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="panel panel-default panel-res">
+        <div class="panel-heading" id="wood-head">Wood</div>
+        <div class="panel-body">
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped active" id="progressBarWood" role="progressbar" aria-valuenow="0"
+                     aria-valuemin="0" aria-valuemax="100" style="width:0%">
+                    <span class="sr-only">70% Complete</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="panel panel-default panel-res">
+        <div class="panel-heading" id="stone-head">Stone</div>
+            <div class="panel-body">
+                <div class="progress">
+                    <div class="progress-bar progress-bar-striped active" id="progressBarStone" role="progressbar" aria-valuenow="0"
+                         aria-valuemin="0" aria-valuemax="100" style="width:0%">
+                    <span class="sr-only">70% Complete</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
