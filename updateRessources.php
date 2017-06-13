d3425bedf5aff314928630478450fc4f19cbc7f6
TheWeindl <theweindl@gmail.com>
2017-06-13 21:41:54 +0200

    update

--- updateRessources.php
+++ updateRessources.php
@@ -1,21 +1,24 @@
 <?php
 Require_once('config.php');
 
-if(!$oMysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE)
+function UpdateRessources()
 {
-	echo("Could not connect to database");
+	if(!$oMysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE)
+	{
+		echo("Could not connect to database");
+	}
+
+	$Result = $oMysqli->query("SELECT lastrefresh FROM userinfo WHERE userID = 2");
+
+	if($Result && $Result->num_rows > 0)
+	{
+		$lastRefresh = mysqli_fetch_assoc($Result);
+		
+		$currentRefresh = date("Y-m-d H:i:s");
+		
+		$TimeDelta = currentRefresh - lastRefresh;
+		echo $TimeDelta;
+		return $TimeDelta;
+	}
 }
-
-$Result = $oMysqli->query("SELECT lastrefresh FROM userinfo WHERE userID = 2");
-
-if($Result && $Result->num_rows > 0)
-{
-	$lastRefresh = mysqli_fetch_assoc($Result);
-	
-	$currentRefresh = date("Y-m-d H:i:s");
-	
-	$TimeDelta = currentRefresh - lastRefresh;
-	echo $TimeDelta;
-}
-
 ?>
