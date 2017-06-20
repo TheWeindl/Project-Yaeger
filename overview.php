ef7943042504478b5be2a8eb1055b2b7f58576ac
prindom <d.prinzensteiner@gmail.com>
2017-06-20 09:54:56 +0200

    moved files to specified folders

--- overview.php
+++ overview.php
@@ -5,7 +5,7 @@
     <title>project-yaeger</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
@@ -120,6 +120,6 @@
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="code.js"></script>
+<script src="assets/js/code.js"></script>
 </body>
 </html>
