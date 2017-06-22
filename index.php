d0a3741f7553bd21a1ce82cfcbc16458c1ababe7
prindom <d.prinzensteiner@gmail.com>
2017-06-22 14:57:29 +0200

    minor changes to the index

--- index.php
+++ index.php
@@ -386,7 +386,7 @@ function showHeaderForLoggedIn() {
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#" data-toggle="modal" data-target="#loginModal">Übersicht</a></li>
+                        <li><a href="index.php">Übersicht</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account <span class="caret"></span></a>
                             <ul class="dropdown-menu">
