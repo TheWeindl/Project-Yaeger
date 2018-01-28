c0c53f6ae0964549d62b667bb224cc4cf3d77b17
TheWeindl <theweindl@gmail.com>
2018-01-28 21:54:38 +0100

    added layout constant

--- src/config/config.php
+++ src/config/config.php
@@ -10,3 +10,126 @@ const DATABASE_NAME = "project_yeager";
 const USERNAME = "root";
 const PASSWORD = "";
 const SERVER = "localhost";
+
+const DATABASE_NAME = "project-yeager";
+const USERNAME = "yeager";
+const PASSWORD = "";
+const SERVER = "localhost";
+
+// rows => cols => values
+const LAYOUT = [
+    0 => [
+        0 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        1 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        2 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        3 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+    ],
+    1 => [
+        0 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        1 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        2 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+    ],
+    2 => [
+        0 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        1 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        2 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        3 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+    ],
+    3 => [
+        0 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        1 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        2 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+    ],
+    4 => [
+        0 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        1 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        2 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        3 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+    ],
+    5 => [
+        0 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        1 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        2 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+    ],
+    6 => [
+        0 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        1 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        2 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+        3 => [
+            "building" => "basic",
+            "level" => 0
+        ],
+    ],
+];
