721cf02b14caf331ac6996296c5e9c756fb0fe4d
prindom <Luzosan1!>
2018-01-28 17:35:31 +0100

    testing

---
+++ src/test/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dprinzensteiner
+ * Date: 28.01.2018
+ * Time: 17:31
+ */
+
+$woodProduction = new WoodProduction();
+var_dump($woodProduction->getCurrentProduction());
